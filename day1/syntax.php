09d1bb28a9dde0a5655dc5166bb50dacf336cfea
Davis-don <davismugoikou@gmail.com>
2025-05-20 13:19:55 +0300

    finished comments and syntax

---
+++ day1/syntax.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is my first application with php</title>
+</head>
+<body>
+<?php
+// First line of code
+echo "heloo word <br/>";
+// Same as below case sensitivity null
+ECHO "Hello world <br/>";
+// Variable declaration
+$name = 20;
+
+// displaying the variable
+echo $name
+
+?>
+</body>
+</html>
